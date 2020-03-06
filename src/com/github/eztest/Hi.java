6d201630cd88764aaa2612a3bd12add1cbc6954a
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-07 02:04:18 +0300

    new branch addLine

--- src/com/github/eztest/Hi.java
+++ src/com/github/eztest/Hi.java
@@ -3,9 +3,12 @@ package com.github.eztest;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("Feature Conflict2");
         System.out.println("Feature Conflict");
-        System.out.println("Feature Conflict");
-        System.out.println("Feature Conflict");
-        System.out.println("Feature Conflict");
+        System.out.println("Feature Conflict2");
+        System.out.println("Feature Conflict2");
+        System.out.println("Feature Conflict2");
+        System.out.println("Feature Conflict2");
+        System.out.println("Feature Conflict2");
     }
 }
